0ef3095de07c620816411e60b14cab6336b86854
Thomas-picki <thomasendalemaw@gmail.com>
2022-08-31 13:42:10 -0700

    0-square_matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    if not matrix:
-        print()
-        return [[item**2 for item in row] for row in matrix]
+    def square_cells(array):
+        result = []
+        for cell in array:
+            result.append(cell ** 2)
+            return result
+        return list(map(square_cells, matrix))
