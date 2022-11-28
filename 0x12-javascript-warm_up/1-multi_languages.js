6791da9f248f91d5fabd3466c459dce043ee9e4e
Thomas-picki <thomasendalemaw@gmail.com>
2022-11-28 02:58:09 -0800

    1-multi_language

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// print 3 line of code
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'Javascript is amazing';
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
