da4f3f7214452824cacbfccd6994173849194d55
Thomas-picki <58863961+Thomas-picki@users.noreply.github.com>
2022-11-28 21:50:38 +0300

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,9 +1,18 @@
 #!/usr/bin/node
-//script that print arguments
-const argv = process.argv[2];
-if (argv === undefined){
-	console.log('No argument');
+// prints the first argument passed to it
+const process = require('process');
+const args = process.argv.slice(2);
+
+// count number of arguments passed without length
+const str = args;
+let len = 0;
+while (str[len] !== undefined) {
+  len += 1;
 }
-else{
-	console.log(argev);
+
+// print argument at index 0
+if (len === 0) {
+  console.log('No argument');
+} else {
+  console.log(str[0]);
 }
