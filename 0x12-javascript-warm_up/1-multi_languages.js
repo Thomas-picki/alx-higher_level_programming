81d19f5dafa914b69afae0e1b54000cdf2b1a0a2
Thomas-picki <thomasendalemaw@gmail.com>
2022-11-28 10:39:36 -0800

    a-multi_language

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
 // print 3 line of code
-const myVar1 = 'C is fun';
-const myVar2 = 'Python is cool';
-const myVar3 = 'Javascript is amazing';
-console.log(myVar1);
-console.log(myVar2);
-console.log(myVar3);
+console.log('Cis fun\n Python is cool\n Javascript is amazing');
